29587b32ddd4e69400f3f6d68af8c1a1cb02fae2
nelox <com.gitlab@nickohristov.de>
2022-02-10 18:20:36 +0100

    php redirect

---
+++ static/secure_redirect.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<body>
+
+<?php 
+  header('Location: https://docs.google.com/presentation/d/1JR53acy5ZsJ-8LKO6u3Pft5nhJXzeKi79SS3h7yyPl8/edit?usp=sharing');
+  exit;
+?> 
+
+</body>
+
+</html>
