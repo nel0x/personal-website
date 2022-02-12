5932dcda6e8e8c4e237039770c249cf6d5203d32
nelox <com.gitlab@nickohristov.de>
2022-02-12 18:00:11 +0100

    set scramble time animation to 60

--- themes/hello-friend-ng/assets/js/scramble.js
+++ themes/hello-friend-ng/assets/js/scramble.js
@@ -14,8 +14,8 @@ class TextScramble {
     for (let i = 0; i < length; i++) {
       const from = oldText[i] || ''
       const to = newText[i] || ''
-      const start = Math.floor(Math.random() * 100)
-      const end = start + Math.floor(Math.random() * 100)
+      const start = Math.floor(Math.random() * 60)
+      const end = start + Math.floor(Math.random() * 60)
       this.queue.push({ from, to, start, end })
     }
     cancelAnimationFrame(this.frameRequest)
